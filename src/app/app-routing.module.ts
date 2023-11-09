fd28d8ab39042ee5818490a796ff06c9ff2982a6
MafeDiaz <maria.diaz7656@soy.sena.edu.co>
2023-11-08 20:35:04 -0500

    nuevo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,7 +27,7 @@ const routes: Routes = [
     path: 'contactanos',
     loadChildren: () => import('./contactanos/contactanos.module').then( m => m.ContactanosPageModule)
   },
-	}
+	
   {
     path: 'contact-detail',
     loadChildren: () => import('./contact-detail/contact-detail.module').then( m => m.ContactDetailPageModule)
@@ -36,11 +36,16 @@ const routes: Routes = [
     path: 'administrador',
     loadChildren: () => import('./administrador/administrador.module').then( m => m.AdministradorPageModule)
   },
+  {
+    path: 'datos-salud',
+    loadChildren: () => import('./datos-salud/datos-salud.module').then( m => m.DatosSaludPageModule)
+  },
+
 
 
 ];
-}
-];
+
+
 
 @NgModule({
   imports: [
